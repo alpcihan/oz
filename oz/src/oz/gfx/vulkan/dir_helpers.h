042ef0a6614c9855f94aa5434361136db3ea7637
Umut Ercan <umut.ercan@tum.de>
2024-12-19 19:56:50 +0100

    add directory helpers

---
+++ oz/src/oz/gfx/vulkan/dir_helpers.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+
+namespace oz {
+#if defined(_WIN32)
+#include <windows.h>
+std::string getExecutablePath() {
+    char result[MAX_PATH];
+    GetModuleFileName(NULL, result, MAX_PATH);
+    return std::string(result);
+}
+#elif defined(__linux__)
+#include <unistd.h>
+#include <limits.h>
+std::string getExecutablePath() {
+    char result[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+    return std::string(result, (count > 0) ? count : 0);
+}
+#elif defined(__APPLE__)
+#include <mach-o/dyld.h>
+std::string getExecutablePath() {
+    char result[PATH_MAX];
+    uint32_t size = sizeof(result);
+    if (_NSGetExecutablePath(result, &size) == 0)
+        return std::string(result);
+    else
+        return std::string();
+}
+#endif
+
+std::string getBuildPath() {
+    return std::string(BUILD_DIR);
+}
+
+std::string getSourcePath() {
+    return std::string(SOURCE_DIR);
+}
+
+}  // namespace oz
